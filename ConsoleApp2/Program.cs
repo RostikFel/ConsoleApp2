530bc4253c86631283847f2e1f5be7d95768e096
Serge <sanichace@gmail.com>
2020-10-15 22:10:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+   
+
